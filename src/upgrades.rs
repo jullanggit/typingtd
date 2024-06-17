1447012f752194fb30a5a4fb5959436c1918bace
jullanggit <jullanggit@proton.me>
2024-06-17 12:49:06 +0200

    removed unneccessary stuff

--- src/upgrades.rs
+++ src/upgrades.rs
@@ -1,25 +1,11 @@
 use bevy::prelude::*;
 
-pub struct UpgradePlugin;
-impl Plugin for UpgradePlugin {
-    fn build(&self, app: &mut App) {
-        // app.init_resource::<Path>()
-        //     .register_type::<(Direction, Path)>()
-        //     .add_systems(Update, follow_path.after(apply_velocity))
-        //     .add_systems(OnEnter(SpritesLoadingStates::Finished), load_path);
-    }
-}
-
-trait TowerUpgrade {}
-trait TowerUpgrades {}
-
 #[derive(Debug, Clone, Reflect, PartialEq)]
 pub enum ArrowTowerUpgrade {
     Piercing(f64),
     Multishot,
     Tracking,
 }
-impl TowerUpgrade for ArrowTowerUpgrade {}
 
 #[derive(Component, Debug, Clone, Reflect, Default)]
 #[reflect(Component)]
@@ -27,4 +13,3 @@ impl TowerUpgrade for ArrowTowerUpgrade {}
 pub struct ArrowTowerUpgrades {
     pub upgrades: Vec<ArrowTowerUpgrade>,
 }
-impl TowerUpgrades for ArrowTowerUpgrades {}
